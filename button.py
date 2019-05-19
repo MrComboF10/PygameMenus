60b010311dafccea4395f5fc1cb344616cccd1ff
MrComboF10 <50202375+MrComboF10@users.noreply.github.com>
2019-05-19 23:46:59 +0100

    Finish button classes

--- button.py
+++ button.py
@@ -1,238 +1,181 @@
 import pygame
 
 
-class PressButtonStatic:
-    def __init__(self, pos, size, text, unpressed_color, pressed_color, font_color, screen_display):
-        self.__position = pos
-        self.__size = size
+class Button:
+    def __init__(self, pos, size, font, font_color, screen_display):
+        self._position = pos
+        self._size = size
+        self._font = font
+        self._font_color = font_color
+        self._screen_display = screen_display
+
+        self._current_mouse_position = (0, 0)
+        self._mouse_over_button = False
+        self._mouse_location_changed = False
+
+    def set_current_mouse_position(self, mouse_position):
+        self._current_mouse_position = mouse_position
+
+    def set_mouse_location_changed(self, change):
+        self._mouse_location_changed = change
+
+    def get_mouse_over_button(self):
+        return self._mouse_over_button
+
+    def get_mouse_location_changed(self):
+        return self._mouse_location_changed
+
+    def verify_mouse_on_button(self):
+        self._mouse_over_button = False
+        if self._position[0] < self._current_mouse_position[0] < self._position[0] + self._size[0]:
+            if self._position[1] < self._current_mouse_position[1] < self._position[1] + self._size[1]:
+                self._mouse_over_button = True
+
+    def draw_button(self, color, text):
+
+        # draw button rectangle
+        pygame.draw.rect(self._screen_display, color, (self._position[0], self._position[1], self._size[0], self._size[1]))
+
+        # get font surface
+        font_surface = self._font.render(text, True, self._font_color)
+
+        # get rect font surface
+        font_surface_rect = font_surface.get_rect()
+
+        # move rect to center of button rectangle
+        font_surface_rect.center = (self._position[0] + (self._size[0] // 2), self._position[1] + (self._size[1] // 2))
+
+        # draw font surface on rectangle
+        self._screen_display.blit(font_surface, font_surface_rect)
+
+
+class PressButton(Button):
+    def __init__(self, pos, size, font, font_color, screen_display, mouse_out_button_color, mouse_over_button_color):
+
+        super().__init__(pos, size, font, font_color, screen_display)
+
+        self._mouse_out_button_color = mouse_out_button_color
+        self._mouse_over_button_color = mouse_over_button_color
+
+    def draw_mouse_out_button(self, text):
+        super().draw_button(self._mouse_out_button_color, text)
+
+    def draw_mouse_over_button(self, text):
+        super().draw_button(self._mouse_over_button_color, text)
+
+
+class PressButtonStatic(PressButton):
+    def __init__(self, pos, size, font, font_color, screen_display, mouse_out_button_color, mouse_over_button_color, text, pointer):
+
+        super().__init__(pos, size, font, font_color, screen_display, mouse_out_button_color, mouse_over_button_color)
+
         self.__text = text
-        self.__pressed_color = pressed_color
-        self.__unpressed_color = unpressed_color
-        self.__screen_display = screen_display
-        self.__font_color = font_color
+        self.__pointer = pointer
 
-        self.__font = pygame.font.SysFont("Arial", 25)
-        self.__current_mouse_position = (0, 0)
-        self.__mouse_on_button = False
-        self.__mouse_location_changed = False
+        self.__pressed = False
 
-    def insert_current_mouse_position(self, mouse_position):
-        self.__current_mouse_position = mouse_position
+    def get_pointer(self):
+        return self.__pointer
 
-    def get_mouse_on_button(self):
-        return self.__mouse_on_button
+    def get_pressed(self):
+        return self.__pressed
 
-    def get_mouse_location_changed(self):
-        return self.__mouse_location_changed
+    def press(self):
+        self.__pressed = True
 
-    def insert_mouse_location_changed(self, changed):
-        self.__mouse_location_changed = changed
+    def release(self):
+        self.__pressed = False
 
-    def verify_mouse_on_button(self):
-        self.__mouse_on_button = False
-        left_position = self.__position[0]
-        right_position = self.__position[0] + self.__size[0]
-        up_position = self.__position[1]
-        down_position = self.__position[1] + self.__size[1]
+    def draw_mouse_out_static_button(self):
+        super().draw_mouse_out_button(self.__text)
 
-        if left_position < self.__current_mouse_position[0] < right_position and up_position < self.__current_mouse_position[1] < down_position:
-            self.__mouse_on_button = True
-
-    def draw_button(self, color):
-
-        # draw button rectangle
-        pygame.draw.rect(self.__screen_display, color, (self.__position[0], self.__position[1], self.__size[0], self.__size[1]))
-
-        # get font surface
-        font_surface = self.__font.render(self.__text, True, self.__font_color)
-
-        # get rect font surface
-        font_surface_rect = font_surface.get_rect()
-
-        # move rect to center of button rectangle
-        font_surface_rect.center = (self.__position[0] + (self.__size[0] // 2), self.__position[1] + (self.__size[1] // 2))
-
-        # draw font surface on rectangle
-        self.__screen_display.blit(font_surface, font_surface_rect)
-
-    def draw_unpressed_button(self):
-        self.draw_button(self.__unpressed_color)
-
-    def draw_pressed_button(self):
-        self.draw_button(self.__pressed_color)
+    def draw_mouse_over_static_button(self):
+        super().draw_mouse_over_button(self.__text)
 
 
-########################################################################################################################
+class PressButtonChangeState(PressButton):
+    def __init__(self, pos, size, font, font_color, screen_display, mouse_out_button_color, mouse_over_button_color, states_text_list):
 
+        super().__init__(pos, size, font, font_color, screen_display, mouse_out_button_color, mouse_over_button_color)
 
-class PressButtonChangeState:
-    def __init__(self, pos, size, states_text_list, unpressed_color, pressed_color, font_color, screen_display):
-        # up left corner position
-        self.__position = pos
+        self.__states_text_list = states_text_list
 
-        self.__size = size
-        self.__state_text_list = states_text_list
-        self.__pressed_color = pressed_color
-        self.__unpressed_color = unpressed_color
-        self.__screen_display = screen_display
-        self.__font_color = font_color
-
-        self.__font = pygame.font.SysFont("Arial", 25)
-        self.__current_mouse_position = (0, 0)
         self.__current_state_text_index = 0
-        self.__mouse_on_button = False
-        self.__mouse_location_changed = False
-
-    def insert_current_mouse_position(self, mouse_position):
-        self.__current_mouse_position = mouse_position
-
-    def get_mouse_on_button(self):
-        return self.__mouse_on_button
-
-    def get_text_index(self):
-        return self.__current_state_text_index
-
-    def get_mouse_location_changed(self):
-        return self.__mouse_location_changed
-
-    def insert_mouse_location_changed(self, changed):
-        self.__mouse_location_changed = changed
-
-    def verify_mouse_on_button(self):
-        self.__mouse_on_button = False
-        left_position = self.__position[0]
-        right_position = self.__position[0] + self.__size[0]
-        up_position = self.__position[1]
-        down_position = self.__position[1] + self.__size[1]
-
-        if left_position < self.__current_mouse_position[0] < right_position and up_position < self.__current_mouse_position[1] < down_position:
-            self.__mouse_on_button = True
-
-    def draw_button(self, color):
-
-        # draw button rectangle
-        pygame.draw.rect(self.__screen_display, color, (self.__position[0], self.__position[1], self.__size[0], self.__size[1]))
-
-        # get font surface
-        current_state_text = self.__state_text_list[self.__current_state_text_index]
-        font_surface = self.__font.render(current_state_text, True, self.__font_color)
-
-        # get rect font surface
-        font_surface_rect = font_surface.get_rect()
-
-        # move rect to center of button rectangle
-        font_surface_rect.center = (self.__position[0] + (self.__size[0] // 2), self.__position[1] + (self.__size[1] // 2))
-
-        # draw font surface on rectangle
-        self.__screen_display.blit(font_surface, font_surface_rect)
-
-    def draw_unpressed_button(self):
-        self.draw_button(self.__unpressed_color)
-
-    def draw_pressed_button(self):
-        self.draw_button(self.__pressed_color)
 
     def add_state_text_index(self):
         self.__current_state_text_index += 1
-        self.__current_state_text_index = self.__current_state_text_index % len(self.__state_text_list)
+        self.__current_state_text_index = self.__current_state_text_index % len(self.__states_text_list)
+
+    def draw_mouse_out_change_state_button(self):
+        super().draw_mouse_out_button(self.__states_text_list[self.__current_state_text_index])
+
+    def draw_mouse_over_change_state_button(self):
+        super().draw_mouse_over_button(self.__states_text_list[self.__current_state_text_index])
 
 
-########################################################################################################################
+class SlideButton(Button):
+    def __init__(self, pos, size, font, font_color, screen_display, bar_width, range_items, mouse_out_bar_color, mouse_over_bar_color, button_color):
 
+        super().__init__(pos, size, font, font_color, screen_display)
 
-class SlideButton:
-    def __init__(self, pos, size, bar_width, range_items, font_color, bar_unpressed_color, bar_pressed_color, button_color, screen_display):
-        self.__position = pos
-        self.__size = size
         self.__bar_width = bar_width
         self.__range_items = range_items
-        self.__font_color = font_color
-        self.__bar_unpressed_color = bar_unpressed_color
-        self.__bar_pressed_color = bar_pressed_color
+        self.__mouse_out_bar_color = mouse_out_bar_color
+        self.__mouse_over_bar_color = mouse_over_bar_color
         self.__button_color = button_color
-        self.__screen_display = screen_display
 
         self.__current_item = range_items[0]
-        self.__font = pygame.font.SysFont("Arial", 25)
-        self.__current_mouse_position = (0, 0)
         self.__current_bar_position = pos
-        self.__mouse_on_button = False
         self.__bar_pressed = False
-        self.__mouse_location_changed = False
 
     def get_bar_is_pressed(self):
         return self.__bar_pressed
 
-    def get_mouse_on_button(self):
-        return self.__mouse_on_button
-
-    def get_mouse_location_changed(self):
-        return self.__mouse_location_changed
-
     def get_current_item(self):
         return self.__current_item
 
-    def insert_current_mouse_position(self, mouse_position):
-        self.__current_mouse_position = mouse_position
+    def press_bar(self):
+        self.__bar_pressed = True
 
-    def insert_bar_is_pressed(self, pressed):
-        self.__bar_pressed = pressed
-
-    def insert_mouse_location_changed(self, changed):
-        self.__mouse_location_changed = changed
-
-    def verify_mouse_on_button(self):
-        self.__mouse_on_button = False
-        if self.__position[0] < self.__current_mouse_position[0] < self.__position[0] + self.__size[0]:
-            if self.__position[1] < self.__current_mouse_position[1] < self.__position[1] + self.__size[1]:
-                self.__mouse_on_button = True
+    def release_bar(self):
+        self.__bar_pressed = False
 
     def calculate_bar_position(self):
-        if self.__current_mouse_position[0] > self.__position[0] + self.__size[0] - self.__bar_width:
-            self.__current_bar_position = (self.__position[0] + self.__size[0] - self.__bar_width, self.__position[1])
+        if self._current_mouse_position[0] > self._position[0] + self._size[0] - self.__bar_width:
+            self.__current_bar_position = (self._position[0] + self._size[0] - self.__bar_width, self._position[1])
 
-        elif self.__current_mouse_position[0] < self.__position[0]:
-            self.__current_bar_position = self.__position
+        elif self._current_mouse_position[0] < self._position[0]:
+            self.__current_bar_position = self._position
 
         else:
-            self.__current_bar_position = (self.__current_mouse_position[0], self.__position[1])
+            self.__current_bar_position = (self._current_mouse_position[0], self._position[1])
 
     def calculate_current_item(self):
-        self.__current_item = self.__range_items[int((self.__current_bar_position[0] - self.__position[0]) * ((len(self.__range_items) - 1) / (self.__size[0] - self.__bar_width)))]
-        # self.__current_item = self.__current_bar_position[0] - self.__position[0]
-        # self.__current_item = len(self.__range_items) // self.__size[0]
+        self.__current_item = self.__range_items[int((self.__current_bar_position[0] - self._position[0]) * ((len(self.__range_items) - 1) / (self._size[0] - self.__bar_width)))]
 
     def draw_font_surface(self):
         # create font surface
-        font_surface = self.__font.render(str(self.__current_item), True, self.__font_color)
+        font_surface = self._font.render(str(self.__current_item), True, self._font_color)
 
         # get font surface rect
         font_surface_rect = font_surface.get_rect()
 
         # move rect to center of button
-        font_surface_rect.center = (self.__position[0] + self.__size[0] // 2, self.__position[1] + self.__size[1] // 2)
+        font_surface_rect.center = (self._position[0] + self._size[0] // 2, self._position[1] + self._size[1] // 2)
 
         # draw font surface
-        self.__screen_display.blit(font_surface, font_surface_rect)
+        self._screen_display.blit(font_surface, font_surface_rect)
 
-    def draw_mouse_on_button(self):
-        pygame.draw.rect(self.__screen_display, self.__button_color, (self.__position[0], self.__position[1], self.__size[0], self.__size[1]))
+    def draw_mouse_over_slide_button(self):
+        pygame.draw.rect(self._screen_display, self.__button_color, (self._position[0], self._position[1], self._size[0], self._size[1]))
 
-        pygame.draw.rect(self.__screen_display, self.__bar_pressed_color, (self.__current_bar_position[0], self.__current_bar_position[1], self.__bar_width, self.__size[1]))
-
-        self.draw_font_surface()
-
-    def draw_mouse_out_button(self):
-        pygame.draw.rect(self.__screen_display, self.__button_color, (self.__position[0], self.__position[1], self.__size[0], self.__size[1]))
-
-        pygame.draw.rect(self.__screen_display, self.__bar_unpressed_color, (self.__current_bar_position[0], self.__current_bar_position[1], self.__bar_width, self.__size[1]))
+        pygame.draw.rect(self._screen_display, self.__mouse_over_bar_color, (self.__current_bar_position[0], self.__current_bar_position[1], self.__bar_width, self._size[1]))
 
         self.draw_font_surface()
 
+    def draw_mouse_out_slide_button(self):
+        pygame.draw.rect(self._screen_display, self.__button_color, (self._position[0], self._position[1], self._size[0], self._size[1]))
 
+        pygame.draw.rect(self._screen_display, self.__mouse_out_bar_color, (self.__current_bar_position[0], self.__current_bar_position[1], self.__bar_width, self._size[1]))
 
-
-
-
-
-
+        self.draw_font_surface()
--- main.py
+++ main.py
@@ -3,39 +3,44 @@ import button
 
 white = (255, 255, 255)
 
+
 def menu_loop():
     pygame.init()
     screen = pygame.display.set_mode((1000, 1000))
     screen.fill(white)
     pygame.display.update()
 
+    font = pygame.font.SysFont("Arial", 25)
+
     button_position_1, button_position_2, button_position_3, button_position_4, button_position_5 = (100, 100), (100, 250), (100, 400), (100, 550), (100, 700)
     button_sizes = (500, 100)
     states_list = ("Fácil", "Médio", "Difícil")
-    pressed_color = (150, 150, 255)
-    unpressed_color = (200, 200, 255)
+    mouse_over_button_color = (150, 150, 255)
+    mouse_out_button_color = (200, 200, 255)
     slide_button_color = (230, 200, 255)
     bar_width = 15
-    range_items = range(4)
+    range_items = range(101)
 
-    change_state_button_1 = button.PressButtonChangeState(button_position_1, button_sizes, states_list, unpressed_color, pressed_color, white, screen)
-    change_state_button_2 = button.PressButtonChangeState(button_position_2, button_sizes, states_list, unpressed_color, pressed_color, white, screen)
-    change_state_button_3 = button.PressButtonChangeState(button_position_3, button_sizes, states_list, unpressed_color, pressed_color, white, screen)
+    change_state_button_1 = button.PressButtonChangeState(button_position_1, button_sizes, font, white, screen, mouse_out_button_color, mouse_over_button_color, states_list)
+    change_state_button_2 = button.PressButtonChangeState(button_position_2, button_sizes, font, white, screen, mouse_out_button_color, mouse_over_button_color, states_list)
+    change_state_button_3 = button.PressButtonChangeState(button_position_3, button_sizes, font, white, screen, mouse_out_button_color, mouse_over_button_color, states_list)
 
-    slide_button_1 = button.SlideButton(button_position_4, button_sizes, bar_width, range_items, white, unpressed_color, pressed_color, slide_button_color, screen)
-    slide_button_2 = button.SlideButton(button_position_5, button_sizes, bar_width, range_items, white, unpressed_color, pressed_color, slide_button_color, screen)
+    slide_button_1 = button.SlideButton(button_position_4, button_sizes, font, white, screen, bar_width, range_items, mouse_out_button_color, mouse_over_button_color, slide_button_color)
+    slide_button_2 = button.SlideButton(button_position_5, button_sizes, font, white, screen, bar_width, range_items, mouse_out_button_color, mouse_over_button_color, slide_button_color)
 
     change_state_buttons = (change_state_button_1, change_state_button_2, change_state_button_3)
     slide_buttons = (slide_button_1, slide_button_2)
     loop_exit = False
 
     for change_button in change_state_buttons:
-        change_button.draw_unpressed_button()
+        change_button.draw_mouse_out_change_state_button()
     pygame.display.update()
 
     for slide_button in slide_buttons:
-        slide_button.draw_mouse_out_button()
+        slide_button.draw_mouse_out_slide_button()
     pygame.display.update()
+
+    change_location = True
 
     while not loop_exit:
         for event in pygame.event.get():
@@ -56,89 +61,86 @@ def menu_loop():
             # ========== change state buttons ==========
             for change_button in change_state_buttons:
 
-                change_button.insert_current_mouse_position(current_mouse_position)
+                change_button.set_current_mouse_position(current_mouse_position)
 
                 # get mouse location before and after to verify if mouse location has changed
-                mouse_location_before = change_button.get_mouse_on_button()
+                mouse_location_before = change_button.get_mouse_over_button()
                 change_button.verify_mouse_on_button()
-                mouse_location_after = change_button.get_mouse_on_button()
+                mouse_location_after = change_button.get_mouse_over_button()
 
                 # verify if mouse location inside or outside button changed
                 if mouse_location_before != mouse_location_after:
-                    change_button.insert_mouse_location_changed(True)
+                    change_button.set_mouse_location_changed(True)
                 else:
-                    change_button.insert_mouse_location_changed(False)
+                    change_button.set_mouse_location_changed(False)
 
                 # verify if mouse is on button
-                if change_button.get_mouse_on_button():
+                if change_button.get_mouse_over_button():
 
                     # verify if mouse was outside button before
                     if change_button.get_mouse_location_changed():
-                        change_button.draw_pressed_button()
+                        change_button.draw_mouse_over_change_state_button()
                         pygame.display.update()
 
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         # mouse left button
                         if event.button == 1:
                             change_button.add_state_text_index()
-                            change_button.draw_pressed_button()
+                            change_button.draw_mouse_over_change_state_button()
                             pygame.display.update()
 
                 else:
                     # verify if mouse was inside before
                     if change_button.get_mouse_location_changed():
-                        change_button.draw_unpressed_button()
+                        change_button.draw_mouse_out_change_state_button()
                         pygame.display.update()
 
             # ========== change slide buttons ==========
 
             for slide_button in slide_buttons:
 
-                slide_button.insert_current_mouse_position(current_mouse_position)
+                slide_button.set_current_mouse_position(current_mouse_position)
 
-                mouse_location_before = slide_button.get_mouse_on_button()
+                mouse_location_before = slide_button.get_mouse_over_button()
                 slide_button.verify_mouse_on_button()
-                mouse_location_after = slide_button.get_mouse_on_button()
+                mouse_location_after = slide_button.get_mouse_over_button()
 
-                if mouse_location_before != mouse_location_after:
-                    slide_button.insert_mouse_location_changed(True)
+                if mouse_location_before != mouse_location_after or change_location:
+                    slide_button.set_mouse_location_changed(True)
+                    change_location = False
                 else:
-                    slide_button.insert_mouse_location_changed(False)
+                    slide_button.set_mouse_location_changed(False)
 
-                if slide_button.get_mouse_on_button():
+                if slide_button.get_mouse_over_button():
                     # verify if left button is pressed inside button bar
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         # mouse left button
                         if event.button == 1:
-                            slide_button.insert_bar_is_pressed(True)
+                            slide_button.press_bar()
 
                     if slide_button.get_mouse_location_changed():
                         slide_button.calculate_current_item()
-                        slide_button.draw_mouse_on_button()
+                        slide_button.draw_mouse_over_slide_button()
                         pygame.display.update()
 
                 else:
                     if slide_button.get_mouse_location_changed():
                         slide_button.calculate_current_item()
-                        slide_button.draw_mouse_out_button()
+                        slide_button.draw_mouse_out_slide_button()
                         pygame.display.update()
 
                 if event.type == pygame.MOUSEBUTTONUP:
                     # mouse left button
                     if event.button == 1:
-                        slide_button.insert_bar_is_pressed(False)
+                        slide_button.release_bar()
+                        # update button draw
+                        change_location = True
 
                 if slide_button.get_bar_is_pressed():
                     slide_button.calculate_bar_position()
-                    print(slide_button.get_current_item())
                     slide_button.calculate_current_item()
-                    slide_button.draw_mouse_on_button()
+                    slide_button.draw_mouse_over_slide_button()
                     pygame.display.update()
-
-
-
-
-
 
 
 menu_loop()
